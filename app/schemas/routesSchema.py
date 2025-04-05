a739db8c211c15d259df17841c267c3530f89f4d
RafaPacheco2003 <115204774+RafaPacheco2003@users.noreply.github.com>
2025-04-05 02:42:02 -0600

    Feature: Route start

--- app/schemas/routesSchema.py
+++ app/schemas/routesSchema.py
@@ -37,6 +37,32 @@ class RouteOut(RouteBase):
 
     class Config:
         from_attributes = True
+
+class RouteStartSchema(BaseModel):
+    id_vehicle_fk: int
+    id_user_fk: int
+    latitude_start: Union[str, float]
+    longitude_start: Union[str, float]
+    start_time: datetime
+    start_km: int
+    image_start_km: str
+    description: str = "Route in progress"
+    
+class RouteStartResponse(BaseModel):
+    id_route: int
+    id_vehicle_fk: int
+    id_user_fk: int
+    description: str
+    latitude_start: str
+    longitude_start: str
+    start_time: datetime
+    start_km: int
+    image_start_km: str
+    name_vehicle: Optional[str] = None
+    name_user: Optional[str] = None
+    
+    class Config:
+        from_attributes = True
         
 
 
